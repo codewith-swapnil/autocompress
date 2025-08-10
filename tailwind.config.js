61b0e8d90769e55106ea52a469b7656433ae42fc
swapnil-subcodevs <swapnil@subcodevs.com>
2025-08-10 23:33:54 +0530

    css working

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Add paths to all of your template files here,
+    // e.g., HTML, JavaScript, etc.
+    "./**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
